6b4bc54294de3bc31b6ec127ff7674a80e379d38
Ozeias Costa <costa.ozeias@gmail.com>
2020-07-04 16:06:47 -0300

    rota de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import UserController from "./app/controllers/UserController";
+import AuthController from "./app/controllers/AuthController";
 
 const router = Router();
 
 router.post("/users", UserController.store);
+router.post("/authenticate", AuthController.authenticate);
 
 export default router;
